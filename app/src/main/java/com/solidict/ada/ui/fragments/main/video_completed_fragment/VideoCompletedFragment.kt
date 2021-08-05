f7cf17d999e9ee789b2ffb161fe18f3bff9a4ebb
Hikmet-Qedirov <hikmet.kadirov.96@gmail.com>
2021-08-05 19:55:35 +0400

    profile button text changed

--- app/src/main/java/com/solidict/ada/ui/fragments/main/video_completed_fragment/VideoCompletedFragment.kt
+++ app/src/main/java/com/solidict/ada/ui/fragments/main/video_completed_fragment/VideoCompletedFragment.kt
@@ -163,7 +163,6 @@ class VideoCompletedFragment : Fragment() {
         val intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE).apply {
             putExtra(MediaStore.EXTRA_DURATION_LIMIT, CAMERA_RECORD_TIME_LIMIT)
             putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
-            putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1)
             flags = (Intent.FLAG_GRANT_READ_URI_PERMISSION
                     or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
             Intent.FLAG_GRANT_WRITE_URI_PERMISSION
@@ -179,7 +178,7 @@ class VideoCompletedFragment : Fragment() {
     }
 
     companion object {
-        private const val CAMERA_RECORD_TIME_LIMIT = 3 * 60
+        private const val CAMERA_RECORD_TIME_LIMIT = 180
     }
 
     override fun onDestroyView() {
